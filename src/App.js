9feee440ed1e85eeef66039fc6b7ab04f4e1f8b4
shankar <shankar.narsim4@gmail.com>
2023-12-11 14:47:18 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          hello lokesh kumar
+          hello this is shankar
         </a>
       </header>
     </div>
